1b253d88a4ab9129bd90e5cf7692596aeac9c4dc
Mladen <blagmash@gmail.com>
2020-10-07 11:33:56 +0200

    redy for deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,55 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { LayoutComponent } from './layout/layout.component';
-import { Page404Component } from './page404/page404.component';
-import { AuthGuard } from './auth/core/auth.guard';
+import { LayoutComponent } from "./layout/layout.component";
+import { Page404Component } from "./page404/page404.component";
+import { AuthGuard } from "./auth/core/auth.guard";
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
   },
   {
-    path: '',
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "",
     component: LayoutComponent,
-    canActivate: [AuthGuard],
+
     children: [
       {
-        path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+        path: "home",
+        loadChildren: () =>
+          import("./home/home.module").then((m) => m.HomeModule),
       },
-      { 
-        path: 'courses', 
-        loadChildren: () => import('./course/course.module').then(m => m.CourseModule),
+      {
+        path: "courses",
+        loadChildren: () =>
+          import("./course/course.module").then((m) => m.CourseModule),
       },
-      { path: 'students', loadChildren: () => import('./student/student.module').then(m => m.StudentModule) },
-      { path: 'teachers', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) },
-    ]
+      {
+        path: "students",
+        loadChildren: () =>
+          import("./student/student.module").then((m) => m.StudentModule),
+      },
+      {
+        path: "teachers",
+        loadChildren: () =>
+          import("./teacher/teacher.module").then((m) => m.TeacherModule),
+      },
+    ],
   },
   {
-    path: '**',
-    component: Page404Component
-
-  }
+    path: "**",
+    component: Page404Component,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/core/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth/core/auth.service";
 
 @Component({
-  selector: 'cm-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  selector: "cm-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.css"],
 })
 export class LayoutComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
     this.authService.logout();
   }
-
 }
